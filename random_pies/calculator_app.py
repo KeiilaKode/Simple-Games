b7dd059454658729e41bc2a9be10530ae850b5be
Aaron Hawthorne <codingwithkeiil@gmail.com>
2026-07-11 06:56:44 -0500

    added style

--- random_pies/calculator_app.py
+++ random_pies/calculator_app.py
@@ -3,123 +3,118 @@ from PyQt6.QtGui import QInputEvent
 from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QLineEdit, QVBoxLayout, QHBoxLayout, QGridLayout
 from PyQt6.QtGui import QFont
 
+# Class takes QWidget as super class
+class CalcApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.app_settings()
+        self.initUI()
+        self.button_events()
 
+    def app_settings(self):
+        self.setGeometry(480, 150, 300, 500)
+        self.setWindowTitle("My Calculator")
+        self.setObjectName("CalculatorBackground")
+        self.setStyleSheet("#CalculatorBackground { background-color: #1e1e2e; }")
 
+    def initUI(self):
+        # Create app objects
+        self.text_box = QLineEdit()
+        self.text_box.setFont(QFont("Helvetica", 32))  # QFont also uses .setStyleSheet() Decorative
+        self.text_box.setStyleSheet("QLineEdit {border:2px solid #ccc; border-radius: 10px;"
+                                    "padding: 10px; background: #444444; color: #54b5fd;}") #333 black
+        self.text_box.setReadOnly(True)
 
-# Settings
-app = QApplication([])
-window = QWidget()
-window.setGeometry(480, 150, 300, 500)
-window.setWindowTitle("My Calculator")
+        # Creates the grid
+        self.grid = QGridLayout()
+        self.buttons = ["7", "8", "9", "/",
+                   "4", "5", "6", "*",
+                   "1", "2", "3", "-",
+                   "0", ".", "=", "+"]
+        # Buttons
+        self.button_clear = QPushButton("Clear")
+        self.button_delete = QPushButton("Delete")
 
-# Create app objects
-text_box = QLineEdit()
-text_box.setFont(QFont("Helvetica", 32)) #QFont also uses .setStyleSheet() Decorative
-
-# Creates the grid
-grid = QGridLayout()
-buttons = ["7", "8", "9", "/",
-               "4", "5", "6", "*",
-               "1", "2", "3", "-",
-               "0", ".", "=", "+"]
-# Buttons
-button_clear = QPushButton("Clear")
-button_delete = QPushButton("Delete")
-
-# Style buttons
-button_clear.setStyleSheet("QPushButton {font: 25pt Comic Sans MS; padding:10px; }")
-button_delete.setStyleSheet("QPushButton {font: 25pt Comic Sans MS; padding:10px; }")
-
-# Design the App, main column container
-master_layout = QVBoxLayout()
-master_layout.addWidget(text_box)
-master_layout.addLayout(grid)
-# Sets the padding around all widgets and outer edge
-master_layout.setContentsMargins(20, 20, 20, 20)
-
-row = QHBoxLayout()
-row.addWidget(button_clear) # , alignment=Qt.AlignmentFlag.AlignLeft
-row.addWidget(button_delete) # , alignment=Qt.AlignmentFlag.AlignRight
-
-# row is added to master_layout last so it puts it at the bottom
-master_layout.addLayout(row)
-
-# Combines all layouts to make the full app window layout
-window.setLayout(master_layout)
-
-# Add the buttons to the Grid, starting at 0, inserts the grid above the row layout made above
-row = 0
-col = 0
-
-# Methods in PyQt to use for logic: .text(), .clear(), eval(), .sender()
-
-# Add functionality
-def button_click():
-    button = app.sender()
-    text = button.text() # gets the text string from button
-
-    if text == "=":
-        # 2 + 2
-        symbol = text_box.text()
-        try:
-            res = eval(symbol)
-            text_box.setText(str(res))
-        except Exception as e:
-            print("Error", e)
-
-    elif text == "Clear":
-        text_box.clear()
-
-    elif text == "Delete":
-        current_value = text_box.text() # 3 + 9....remove the 9
-        text_box.setText(current_value[:-1])
-
-    else:
-        current_value = text_box.text()
-        text_box.setText(current_value + text)
-
-# Create button for each element in button list
-for text in buttons:
-    # Creates a button every loop, applies text from list to it
-    button = QPushButton(text)
-    button.clicked.connect(button_click)
-    # Styles the rest of the button's font
-    button.setStyleSheet("QPushButton {font: 25pt Comic Sans MS; padding:10px; }")
-    # Create an event here
-    grid.addWidget(button, row, col)
-    col += 1
-    if col > 3:
+        self.button_clear.setStyleSheet("QPushButton {font: 25pt Helvetica; border: 3px solid #ccc; border-radius: 15px; padding:10px; }")
+        self.button_delete.setStyleSheet("QPushButton {font: 25pt Helvetica; border: 3px solid #ccc; border-radius: 15px; padding:10px; }")
+        # Add the buttons to the Grid, starting at 0, inserts the grid above the row layout made above
+        row = 0
         col = 0
-        row +=1
+        # Create button for each element in button list
+        for text in self.buttons:
+            # Creates a button every loop, applies text from list to it
+            button = QPushButton(text)
+            button.clicked.connect(self.button_click)
+            # Styles the rest of the button's font *************
+            button.setStyleSheet(self.get_button_style())
+            # Create an event here
+            self.grid.addWidget(button, row, col)
+            col += 1
+            if col > 3:
+                col = 0
+                row += 1
 
-button_clear.clicked.connect(button_click)
-button_delete.clicked.connect(button_click)
+        # Design the App, main column container
+        master_layout = QVBoxLayout()
+        master_layout.addWidget(self.text_box)
+        master_layout.addLayout(self.grid)
+        master_layout.setContentsMargins(20, 20, 20, 20)  # Sets the padding around all widgets and outer edge
+        row = QHBoxLayout()
+        row.addWidget(self.button_clear)  # , alignment=Qt.AlignmentFlag.AlignLeft
+        row.addWidget(self.button_delete)  # , alignment=Qt.AlignmentFlag.AlignRight
+        master_layout.addLayout(row)
+        # Combines all layouts to make the full app window layout
+        self.setLayout(master_layout)
 
-# Styles the rest of the button's font
 
+    # this is pink in hexadecimal #fd00d6: and #ff99ff
+    # dark grey #444444: light grey #777777 teal #ff99ff
+    def get_button_style(self):
+        return """
+    QPushButton {
+        font: 18pt Helvetica;
+        border: 3px solid #444444;
+        border-radius: 15px;
+        background-color: #ffffff;
+        color: #333;
+        padding: 15px;
+        
+    }
+    
+    """
 
+    def button_events(self):
+        self.button_clear.clicked.connect(self.button_click)
+        self.button_delete.clicked.connect(self.button_click)
 
+    # Add functionality
+    def button_click(self):
+        button = app.sender()
+        text = button.text()  # gets the text string from button
+        if text == "=":
+            # 2 + 2
+            symbol = self.text_box.text()
+            try:
+                res = eval(symbol)
+                self.text_box.setText(str(res))
+            except Exception as e:
+                print("Error", e)
+
+        elif text == "Clear":
+            self.text_box.clear()
+
+        elif text == "Delete":
+            current_value = self.text_box.text()  # 3 + 9....remove the 9
+            self.text_box.setText(current_value[:-1])
+
+        else:
+            current_value = self.text_box.text()
+            self.text_box.setText(current_value + text)
 
 
 if __name__ == "__main__":
+    app = QApplication([])
+    window = CalcApp()
     window.show()
     app.exec()
 
-
-# Take grid layout, add button, to this row and this column, needs 18 buttons
-#grid.addWidget(button, row, col)
-
-# Make row/column index of 5: # first row/cols, index start at 0
-# To add the number 5 to button, it would be (row2,col1)
-
-# row = 0
-# col = 0
-# def make_grid():
-#     for button_text in buttons:
-#         button = QPushButton(button_text)
-#         button.clicked.connect(button_click)
-#         grid.addWidget(button, row, col)
-#         col += 1
-#         if col > 3:
-#             col = 0
-#             row += 1
