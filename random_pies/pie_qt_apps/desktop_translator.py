ec09244b93ddafd6f03e379152874f359b173835
Aaron Hawthorne <codingwithkeiil@gmail.com>
2026-07-11 23:46:35 -0500

    changed font color

--- random_pies/pie_qt_apps/desktop_translator.py
+++ random_pies/pie_qt_apps/desktop_translator.py
@@ -14,7 +14,7 @@ class TranslatorApp(QWidget):
 
     def setup(self):
         self.setGeometry(375, 75, 900, 725)
-        self.setWindowTitle("Translator 2026")
+        self.setWindowTitle("Python's Translator")
         self.initUI()
         self.events()
 
@@ -26,43 +26,43 @@ class TranslatorApp(QWidget):
         self.title = QLabel("""   PyLateR
 TransMitteR""")
         self.title.setFont(QFont("Lucida Handwriting", 30)) # Lucida Handwriting
-        self.title.setStyleSheet("QLabel {padding: 2px; color: #54b5fd;}")
+        self.title.setStyleSheet("QLabel {padding: 2px; color: #05b5fd;}")
         self.input_option = QComboBox()
         self.input_option.setFont(QFont("Helvetica", 10))
         self.input_option.setStyleSheet("QComboBox {border:3px solid #ff99ff; border-radius: 10px;"
-                                     "padding: 6px; background: #444444; color: #54b5fd;}")
+                                     "padding: 6px; background: #333333; color: #05b5fd;}")
 
         self.output_option = QComboBox()
         self.output_option.setFont(QFont("Helvetica", 10))
         self.output_option.setStyleSheet("QComboBox {border:3px solid #ff99ff; border-radius: 10px;"
-                                     "padding: 6px; background: #444444; color: #54b5fd;}")
+                                     "padding: 6px; background: #333333; color: #05b5fd;}")
 
         self.trans_button = QPushButton("Translate")
         self.trans_button.setFont(QFont("Helvetica", 20))
         self.trans_button.setStyleSheet("QPushButton {border:3px solid #ff99ff; border-radius: 10px;"
-                                     "padding: 10px; background: #444444; color: #54b5fd;}")
+                                     "padding: 10px; background: #333333; color: #05b5fd;}")
 
         self.clear_button = QPushButton("Clear")
         self.clear_button.setFont(QFont("Helvetica", 20))
         self.clear_button.setStyleSheet("QPushButton {border:3px solid #ff99ff; border-radius: 10px;"
-                                     "padding: 10px; background: #444444; color: #54b5fd;}")
+                                     "padding: 10px; background: #333333; color: #05b5fd;}")
 
         # Right side
         self.input_box = QTextEdit()
         self.input_box.setPlaceholderText("Enter Passage here...")
         self.input_box.setFont(QFont("Helvetica", 20))
         self.input_box.setStyleSheet("QTextEdit {border:3px solid #ff99ff; border-radius: 10px;"
-                                     "padding: 10px; background: #444444; color: #54b5fd;}")
+                                     "padding: 10px; background: #333333; color: #05b5fd;}")
         self.input_box.setReadOnly(False)
         self.reverse_button = QPushButton("Reverse")
         self.reverse_button.setFont(QFont("Helvetica", 20))
         self.reverse_button.setStyleSheet("QPushButton {border:3px solid #ff99ff; border-radius: 10px;"
-                                     "padding: 10px; background: #444444; color: #54b5fd;}")
+                                     "padding: 10px; background: #333333; color: #05b5fd;}")
 
         self.output_box = QTextEdit()
         self.output_box.setFont(QFont("Helvetica", 20))
         self.output_box.setStyleSheet("QTextEdit {border:3px solid #ff99ff; border-radius: 10px;"
-                                      "padding: 10px; background: #444444; color: #54b5fd;}")
+                                      "padding: 10px; background: #333333; color: #05b5fd;}")
         #self.output_box.setReadOnly(True)
         self.input_option.addItems(values)
         self.output_option.addItems(values)
